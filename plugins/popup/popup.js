9a2d87a3801a7602ce4217ae00030a251cba1841
tuxingsheng <tuxingsheng88@163.com>
2016-08-25 11:30:32 +0800

    优化popup

--- plugins/popup/popup.js
+++ plugins/popup/popup.js
@@ -4,6 +4,10 @@
  * popup逻辑代码
  * */
 (function (win, doc) {
+
+    /*
+     * 工具方法
+     * */
     var util = {
         /*
          * @name isArray
@@ -49,6 +53,19 @@
             return true;
         },
         /*
+         * @name extend
+         * @type function
+         * @explain 复制对象
+         * */
+        extend: function (to, from) {
+            var keys = Object.keys(from);
+            var i = keys.length;
+            while (i--) {
+                to[keys[i]] = from[keys[i]];
+            }
+            return to;
+        },
+        /*
          * @name each
          * @type function
          * @explain each循环
@@ -72,22 +89,75 @@
         }
     };
 
-    var aPopups = document.querySelectorAll('[data-popup]');
-    var popupStatus = {};
+    function Popup() {
 
-    if (!util.isEmpty(aPopups)) {
-        util.each(aPopups, function (i, e) {
-            e.addEventListener('touchstart', function () {
-                if (typeof popupStatus[this.dataset.popup] == 'undefined' || !popupStatus[this.dataset.popup]) {
-                    popupStatus[this.dataset.popup] = true;
-                    var openPop = document.querySelector(this.dataset.popup);
-                    openPop && openPop.classList.add('ue-active');
-                } else {
-                    popupStatus[this.dataset.popup] = false;
-                    var closePop = document.querySelector(this.dataset.popup);
-                    closePop && closePop.classList.remove('ue-active');
-                }
-            }, false);
-        })
+        // 状态系统
+        this.popupStatus = {};
+        // popup对象
+        this.popupList = [];
+
+        this.init();
+    }
+
+    /*
+     * 初始化
+     * */
+    Popup.prototype.init = function () {
+        var self = this,
+            aP = document.querySelectorAll('[data-popup]');
+
+        if (!util.isEmpty(aP)) {
+            util.each(aP, function (i, e) {
+                self.popupList.push(e);
+                self.bindEvent(e);
+            })
+        }
+    };
+
+    /*
+     * js打开popup
+     * */
+    Popup.prototype.open = function (popupId, trigger) {
+        var pop = document.querySelector(popupId);
+
+        if (pop) {
+            trigger = util.isString(trigger) ? document.querySelector(trigger) : trigger;
+            trigger.setAttribute('data-popup', popupId);
+            this.popupList.push(pop);
+            this.popover(trigger);
+        }
+    };
+
+    /*
+     * 绑定触发事件
+     * */
+    Popup.prototype.bindEvent = function (pop) {
+        pop.addEventListener('touchstart', function () {
+            this.popover(pop);
+        }.bind(this), false);
+    };
+
+    /*
+     * 判断关闭 or 开启
+     * */
+    Popup.prototype.popover = function (pop) {
+        if (typeof this.popupStatus[pop.dataset.popup] == 'undefined' || !this.popupStatus[pop.dataset.popup]) {
+            this.popupStatus[pop.dataset.popup] = true;
+            var openPop = document.querySelector(pop.dataset.popup);
+            openPop && openPop.classList.add('ue-active');
+        } else {
+            this.popupStatus[pop.dataset.popup] = false;
+            var closePop = document.querySelector(pop.dataset.popup);
+            closePop && closePop.classList.remove('ue-active');
+        }
+    };
+
+
+    if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+        define(Popup);
+    } else if (typeof module !== 'undefined' && module.exports) {
+        module.exports = new Popup();
+    } else {
+        win.popup = new Popup();
     }
 })(window, document);
