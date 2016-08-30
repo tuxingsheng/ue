21b95af49d6ccd1e31076e7108a291518da01b79
tuxingsheng <tuxingsheng88@163.com>
2016-08-30 12:49:46 +0800

    picker支持多条数据展示

--- plugins/picker/picker.js
+++ plugins/picker/picker.js
@@ -5,6 +5,31 @@
 
     'use strict';
 
+    var util = {
+        /*
+         * @name each
+         * @type function
+         * @explain each循环
+         * */
+        each: function (obj, fn) {
+            if (obj) {
+                var i = 0;
+                if (obj.length) {
+                    for (var n = obj.length; i < n; i++) {
+                        if (fn(i, obj[i]) === false)
+                            break
+                    }
+                } else {
+                    for (i in obj) {
+                        if (obj.hasOwnProperty(i) && fn(i, obj[i]) === false) {
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    };
+
     function Picker(options) {
 
         if (typeof options == 'undefined') {
@@ -24,7 +49,9 @@
             items: [],
             spaceNum: 2,
             probeType: 3,
-            field: 'name',
+            apart: '',
+            show: 'text',
+            export: 'value',
             onScrollEnd: function () {
 
             },
@@ -38,14 +65,8 @@
                 this.defaults[o] = options[o];
             }
         }
-
-        this.scrollEndData = {
-            index: 0,
-            data: this.defaults.items[0]
-        };
-
         // 初始化HTML结构
-        _init.call(this);
+        this._init();
     }
 
     /*
@@ -58,12 +79,11 @@
         this.pickerWrapper.style.transform = 'translate3d(0, ' + this.pickerWrapper.clientHeight + 'px, 0)';
         var self = this;
         setTimeout(function () {
-            self.pickerScroll.scrollTo(0, 0);
-            _switchActiveClass(0, self);
-            self.scrollEndData = {
-                index: 0,
-                data: self.defaults.items[0]
-            };
+            util.each(self.pickerScroll, function (i, e) {
+                e.scrollTo(0, 0);
+                self.scrollIndex[i] = 0;
+                self._switchActiveClass(i, 0);
+            });
         }, 500);
     };
 
@@ -81,49 +101,29 @@
         this.pickerWrapper.style.transform = 'translate3d(0, 0, 0)';
     };
 
-    //初始化组件结构
-    function _init() {
-
-        //所有选项信息
-        //var items = this.defaults.items;
-        //空位添加使首选项居中
-        var spaces = '',
-            options = '',
-            self = this;
-
-        for (var x = 0; x < this.defaults.spaceNum; x++) {
-            spaces += '<li class="ue-picker-item"></li>';
-        }
-
-        for (var i = 0; i < this.defaults.items.length; i++) {
-            options += '<li class="ue-picker-item">' + (this.defaults.items[i][this.defaults.field] ? this.defaults.items[i][this.defaults.field] : '') + '</li>';
-        }
-
-        options = spaces + options + spaces;
-
-        this.pickerList.innerHTML = '<ul>' + options + '</ul>';
-        this.pickerItems = this.pickerList.querySelectorAll('.ue-picker-item');
-        this.pickerIndicator = this.pickerWrapper.querySelector('.ue-picker-indicator');
-
-        // 初始化滚动区域高度
-        this.pickerList.style.height = (this.defaults.spaceNum * 2 + 1) * this.defaults.itemWidth + 'px';
-        // 初始化indicator高度
-        this.pickerIndicator.style.height = this.defaults.itemWidth + 'px';
-        // 初始化高亮
-        this.pickerItems[this.defaults.spaceNum].classList.add('ue-picker-active');
-        // 初始化item的高度
-        for (var k = 0; k < this.pickerItems.length; k++) {
-            this.pickerItems[k].style.height = this.pickerItems[k].style.lineHeight = this.defaults.itemWidth + 'px';
-        }
+    Picker.prototype._init = function () {
+        // 创建DOM结构
+        this._createFlex();
+        // 初始化当前定位索引
+        this._scrollIndexInit();
         // 初始化iscroll
-        this.pickerScroll = new IScroll('.ue-picker-list', {
-            probeType: self.defaults.probeType
-        });
+        this._createPickerScroll();
+        // 初始化绑定事件处理
+        this._bindEvent();
+        // 初始化的时候隐藏
+        this.hide();
+    };
 
-        var getActiveIndex = function (posY) {
-            var index = -Math.round(posY / self.defaults.itemWidth);
-            if (index > self.defaults.items.length - 1) {
-                index = self.defaults.items.length - 1;
+    Picker.prototype._createPickerScroll = function () {
+        this.pickerScroll = {};
+        var pickerFlexs = document.querySelectorAll('.ue-picker-list-flex'), i = 0, self = this;
+
+        var getActiveIndex = function (i, posY) {
+            var index = -Math.round(posY / self.defaults.itemWidth),
+                values = self.defaults.items[i].values.length - 1;
+
+            if (index > values) {
+                index = values;
             }
             if (index <= 0) {
                 index = 0;
@@ -131,49 +131,124 @@
             return index;
         };
 
-        this.pickerScroll.on('scrollEnd', function () {
-            var index = getActiveIndex(this.y);
-            _switchActiveClass(index, self);
-            this.scrollTo(0, -index * self.defaults.itemWidth);
-            self.scrollEndData = {
-                index: index,
-                data: self.defaults.items[index]
-            };
-            self.defaults.onScrollEnd(self.scrollEndData);
+        var scrollEnd = function (i) {
+            var index = getActiveIndex(i, self.pickerScroll[i].y);
+            self._switchActiveClass(i, index);
+            self.scrollIndex[i] = index;
+            self.defaults.onScrollEnd(self._scrollEndData());
+        };
+
+        var scrollLoad = function (i) {
+            var index = getActiveIndex(i, self.pickerScroll[i].y);
+            self._switchActiveClass(i, index);
+        };
+
+        // 初始化iscroll
+        util.each(pickerFlexs, function (i, e) {
+            self.pickerScroll[i] = new IScroll(e, {
+                probeType: self.defaults.probeType,
+                snap: 'li'
+            });
+
+            self.pickerScroll[i].on('scrollEnd', function () {
+                scrollEnd(i)
+            });
+
+            self.pickerScroll[i].on('scroll', function () {
+                scrollLoad(i)
+            });
         });
 
-        this.pickerScroll.on('scroll', function () {
-            var index = getActiveIndex(this.y);
-            _switchActiveClass(index, self);
-        });
+    };
 
+    Picker.prototype._scrollIndexInit = function () {
+        this.scrollIndex = [];
+        for (var i = 0; i < this.defaults.items.length; i++) {
+            this.scrollIndex.push(0);
+        }
+    };
+
+    Picker.prototype._scrollData = function (x, y) {
+        return this.defaults.items[x].values[y][this.defaults.export];
+    };
+
+    Picker.prototype._scrollEndData = function () {
+        var result = '', length = this.defaults.items.length, i = 0;
+        for (; i < length; i++) {
+            result += this._scrollData(i, this.scrollIndex[i]) + (i == length - 1 ? '' : this.defaults.apart);
+        }
+        return result;
+    };
+
+    Picker.prototype._bindEvent = function () {
         document.addEventListener('touchmove', function (e) {
             e.preventDefault();
         }, false);
-
         // 添加取消和确定的监听
         document.querySelector('.ue-picker-cancel').addEventListener('click', function () {
-            self.hide();
-        }, false);
+            this.hide();
+        }.bind(this), false);
         document.querySelector('.ue-picker-confirm').addEventListener('click', function () {
-            self.hide();
-            self.defaults.onConfirm(self.scrollEndData);
-        }, false);
-        // 添加遮罩的监听
+            this.hide();
+            this.defaults.onConfirm(this._scrollEndData());
+        }.bind(this), false);
+        // 添加遮罩的事件监听
         this.pickerCover.addEventListener('click', function () {
-            self.hide();
-        }, false);
-        //初始化的时候隐藏
-        this.hide();
-    }
+            this.hide();
+        }.bind(this), false);
+    };
 
-    // 改变active的item
-    function _switchActiveClass(index, self) {
-        for (var i = 0; i < self.pickerItems.length; i++) {
-            self.pickerItems[i].classList.remove('ue-picker-active');
+    Picker.prototype._createSpace = function () {
+        var dom = '', i = 0;
+        for (; i < this.defaults.spaceNum; i++) {
+            dom += '<li class="ue-picker-item"></li>';
         }
-        self.pickerItems[index + self.defaults.spaceNum].classList.add('ue-picker-active');
-    }
+        return dom;
+    };
+
+    Picker.prototype._createItem = function (index) {
+        var dom = '', i = 0;
+        for (; i < this.defaults.items[index]['values'].length; i++) {
+            var item = this.defaults.items[index]['values'][i][this.defaults.show];
+            dom += '<li class="ue-picker-item">' + item + '</li>';
+        }
+        return dom;
+    };
+
+    Picker.prototype._createFlex = function () {
+        var i = 0;
+        this.pickerItems = {};
+        for (; i < this.defaults.items.length; i++) {
+            var flex = document.createElement('div');
+            flex.className = 'ue-picker-list-flex';
+            flex.innerHTML = '<ul>' + this._createSpace() + this._createItem(i) + this._createSpace() + '</ul>';
+            this.pickerList.appendChild(flex);
+            this.pickerItems[i] = flex.querySelectorAll('.ue-picker-item');
+            // 初始化pickerItems高亮
+            this.pickerItems[i][this.defaults.spaceNum].classList.add('ue-picker-active');
+            // 初始化pickerItems的高度
+            for (var k = 0; k < this.pickerItems[i].length; k++) {
+                this.pickerItems[i][k].style.height = this.pickerItems[i][k].style.lineHeight = this.defaults.itemWidth + 'px';
+            }
+            // 初始化indicator高度
+            this._indicatorInit();
+        }
+    };
+
+    Picker.prototype._indicatorInit = function () {
+        this.pickerIndicator = this.pickerWrapper.querySelector('.ue-picker-indicator');
+        this.pickerIndicator.style.height = this.defaults.itemWidth + 'px';
+        // 初始化滚动区域高度
+        this.pickerList.style.height = (this.defaults.spaceNum * 2 + 1) * this.defaults.itemWidth + 'px';
+    };
+
+    Picker.prototype._switchActiveClass = function (index, sIndex) {
+        var pickerItems = this.pickerItems[index], i = 0;
+        for (; i < pickerItems.length; i++) {
+            pickerItems[i].classList.remove('ue-picker-active');
+        }
+        pickerItems[sIndex + this.defaults.spaceNum].classList.add('ue-picker-active');
+    };
 
     if (typeof exports === 'object') module.exports = Picker;
     else if (typeof define === 'function' && define.amd) define([], function () {
