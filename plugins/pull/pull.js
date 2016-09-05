7b1765205537304c92c6ddbdcda45e5cc42d516e
tuxingsheng <tuxingsheng88@163.com>
2016-09-05 15:52:36 +0800

    优化pull

--- plugins/pull/pull.js
+++ plugins/pull/pull.js
@@ -27,12 +27,16 @@
             scroll: 'appScroll',
             limit: 44,
             maxTime: 10,
+            pullUp: false,
             pullDown: false,
-            //autoInit: false,
-            pullInitName: '下拉可以刷新',
+            pullInitDownName: '下拉可以刷新',
+            pullInitUpName: '上拉可以刷新',
             pullStartName: '释放立即刷新',
-            pullLoadName: '正在刷新...',
-            pullEndName: '没有更多数据了',
+            pullLoadingName: '正在刷新...',
+            pullClearName: '没有更多数据了',
+            pullUpSuccess: function () {
+
+            },
             pullDownSuccess: function () {
 
             }
@@ -45,6 +49,7 @@
         // 1 --- 释放可刷新
         // 2 --- 正在刷新
         // 3 --- 没有更多数据了
+        this.pullUpState = 0;
         this.pullDownState = 0;
 
         if (!ue.iscrollList[this.defaults.scroll]) {
@@ -55,12 +60,26 @@
         this._init();
     }
 
+    /*
+     * 初始化pull
+     * */
     Pull.prototype._init = function () {
-        this._queryElement();
+        this._pullElement();
         this._bindEvent();
     };
 
-    Pull.prototype.scrollerEnd = function () {
+    /*
+     * pullUp结束
+     * */
+    Pull.prototype.scrollerUpEnd = function () {
+        this._pullUpState(3);
+        this.pull.refresh();
+    };
+
+    /*
+     * pullDown结束
+     * */
+    Pull.prototype.scrollerDownEnd = function () {
         this._pullDownState(3);
         this.pull.refresh();
     };
@@ -68,7 +87,16 @@
     /*
      * 如果不存在DOM结构，就动态创建
      * */
-    Pull.prototype._queryElement = function () {
+    Pull.prototype._pullElement = function () {
+        if (this.defaults.pullUp) {
+            this.pullUp = this.pull.wrapper.querySelector('.ue-pull-up');
+            if (this.pullUp) {
+                this.pullUpIcon = this.pullUp.querySelector('.ue-pull-up-icon');
+                this.pullUpLabel = this.pullUp.querySelector('.ue-pull-up-label');
+            } else {
+                this._createPullUp();
+            }
+        }
         if (this.defaults.pullDown) {
             this.pullDown = this.pull.wrapper.querySelector('.ue-pull-down');
             if (this.pullDown) {
@@ -78,6 +106,21 @@
                 this._createPullDown();
             }
         }
+    };
+
+    /*
+     * 动态创建pullUp
+     * */
+    Pull.prototype._createPullUp = function () {
+        this.pullUp = document.createElement('div');
+        this.pullUpIcon = document.createElement('span');
+        this.pullUpLabel = document.createElement('span');
+        this.pullUp.className = 'ue-pull-up';
+        this.pullUpIcon.className = 'ue-pull-up-icon';
+        this.pullUpLabel.className = 'ue-pull-up-label';
+        this.pullUp.appendChild(this.pullUpIcon);
+        this.pullUp.appendChild(this.pullUpLabel);
+        this.pull.wrapper.appendChild(this.pullUp);
     };
 
     /*
@@ -104,40 +147,47 @@
         this.pull.on('refresh', function () {
             count = 0;
             clearInterval(timer);
-            if (self.pullDownState != 3) {
+            if (self.pullDownState != 3 && self.defaults.pullDown) {
                 self._pullDownState(0);
+            }
+            if (self.pullUpState != 3 && self.defaults.pullUp) {
+                self._pullUpState(0);
             }
         });
 
         this.pull.on('scroll', function () {
-            if (self.defaults.pullDown) {
+            if (this.y > self.defaults.limit && self.defaults.pullDown) {
                 if (self.pullDownState == 2 || self.pullDownState == 3) {
                     return false;
                 }
                 self._pullDownState(this.y > self.defaults.limit ? 1 : 0);
             }
-        });
-
-        this.pull.on('scrollEnd', function () {
-            // scrollEnd
+            if (this.maxScrollY > this.y && self.defaults.pullUp) {
+                if (self.pullUpState == 2 || self.pullUpState == 3) {
+                    return false;
+                }
+                self._pullUpState(this.maxScrollY - self.defaults.limit > this.y ? 1 : 0);
+            }
         });
 
         this.pull.scroller.addEventListener('touchend', function () {
-            if (self.defaults.pullDown) {
+            if (self.pull.y > self.defaults.limit && self.defaults.pullDown) {
                 if (self.pullDownState == 1) {
                     self._pullDownState(2);
                     self.defaults.pullDownSuccess();
-                    timer = setInterval(function () {
-                        count++;
-                        if (count >= self.defaults.maxTime) {
-                            count = 0;
-                            clearInterval(timer);
-                            self._pullDownState(0);
-                        }
-                    }, 1000);
+                }
+            }
+            if (self.pull.maxScrollY > self.pull.y && self.defaults.pullUp) {
+                if (self.pullUpState == 1) {
+                    self._pullUpState(2);
+                    self.defaults.pullUpSuccess();
                 }
             }
         }, false);
+
+        this.pull.on('scrollEnd', function () {
+            // scrollEnd...
+        });
     };
 
     /*
@@ -150,7 +200,7 @@
                 this.pullDownState = 0;
                 this.pull.scroller.style.top = '0px';
                 this.pullDownIcon.style.display = 'inline-block';
-                this.pullDownLabel.innerText = this.defaults.pullInitName;
+                this.pullDownLabel.innerText = this.defaults.pullInitDownName;
                 this.pullDownIcon.className = 'ue-pull-down-icon ue-icon ue-icon-pulldown';
                 break;
             // 允许加载的状态
@@ -166,7 +216,7 @@
                 this.pullDownState = 2;
                 this.pull.scroller.style.top = '44px';
                 this.pullDownIcon.style.display = 'inline-block';
-                this.pullDownLabel.innerText = this.defaults.pullLoadName;
+                this.pullDownLabel.innerText = this.defaults.pullLoadingName;
                 this.pullDownIcon.className = 'ue-pull-down-icon ue-spinner';
                 break;
             // 没有更多数据了
@@ -174,7 +224,46 @@
                 this.pullDownState = 3;
                 this.pull.scroller.style.top = '0px';
                 this.pullDownIcon.style.display = 'none';
-                this.pullDownLabel.innerText = this.defaults.pullEndName;
+                this.pullDownLabel.innerText = this.defaults.pullClearName;
+                break;
+        }
+    };
+
+    /*
+     * pullUp状态设置
+     * */
+    Pull.prototype._pullUpState = function (index) {
+        switch (index) {
+            // 初始化状态
+            case 0:
+                this.pullUpState = 0;
+                this.pull.scroller.style.top = '0px';
+                this.pullUpIcon.style.display = 'inline-block';
+                this.pullUpLabel.innerText = this.defaults.pullInitUpName;
+                this.pullUpIcon.className = 'ue-pull-up-icon ue-icon ue-icon-pulldown ue-rotate-180';
+                break;
+            // 允许加载的状态
+            case 1:
+                this.pullUpState = 1;
+                this.pull.scroller.style.top = '0px';
+                this.pullUpIcon.style.display = 'inline-block';
+                this.pullUpLabel.innerText = this.defaults.pullStartName;
+                this.pullUpIcon.className = 'ue-pull-up-icon ue-icon ue-icon-pulldown';
+                break;
+            // 加载中状态
+            case 2:
+                this.pullUpState = 2;
+                this.pull.scroller.style.top = '-44px';
+                this.pullUpIcon.style.display = 'inline-block';
+                this.pullUpLabel.innerText = this.defaults.pullLoadingName;
+                this.pullUpIcon.className = 'ue-pull-up-icon ue-spinner';
+                break;
+            // 没有更多数据了
+            case 3:
+                this.pullUpState = 3;
+                this.pull.scroller.style.top = '0px';
+                this.pullUpIcon.style.display = 'none';
+                this.pullUpLabel.innerText = this.defaults.pullClearName;
                 break;
         }
     };
